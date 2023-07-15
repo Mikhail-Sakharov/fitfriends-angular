106dee31bf0456965ea816eaa5a24b2a12cbad25
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-07-15 17:24:20 +0300

    feat: coach-questionnaire: отрисовывает инпуты выбора типов тренировки, добавляет обработчик

--- src/app/pages/sign-up-questionnaire-coach/sign-up-questionnaire-coach.component.ts
+++ src/app/pages/sign-up-questionnaire-coach/sign-up-questionnaire-coach.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TrainingType} from 'src/app/app.constant';
+import {CoachQuestionnaireStateService} from 'src/app/services/coach-questionnaire-state/coach-questionnaire-state.service';
 import {SignUpStateService} from 'src/app/services/sign-up-state.service';
 
 @Component({
@@ -6,10 +8,28 @@ import {SignUpStateService} from 'src/app/services/sign-up-state.service';
   templateUrl: './sign-up-questionnaire-coach.component.html',
   styleUrls: ['./sign-up-questionnaire-coach.component.css']
 })
-export class SignUpQuestionnaireCoachComponent {
+export class SignUpQuestionnaireCoachComponent implements OnInit {
+  public trainingTypes = Object.values(TrainingType);
+  public selectedTrainigTypes: TrainingType[] = [];
+
   constructor(
-    private signUpStateService: SignUpStateService
+    private signUpStateService: SignUpStateService,
+    private coachQuestionnaireStateService: CoachQuestionnaireStateService
   ) {}
+
+  public ngOnInit(): void {
+    this.selectedTrainigTypes = this.coachQuestionnaireStateService.selectedTrainigTypes;
+  }
+
+  public handleTrainingTypeInputChange(trainingType: TrainingType) {
+    if (this.selectedTrainigTypes.includes(trainingType)) {
+      this.selectedTrainigTypes = this.selectedTrainigTypes.filter((type) => type !== trainingType);
+      this.coachQuestionnaireStateService.selectedTrainigTypes = this.coachQuestionnaireStateService.selectedTrainigTypes.filter((type) => type !== trainingType);
+    } else {
+      this.selectedTrainigTypes = [...this.selectedTrainigTypes, trainingType];
+      this.coachQuestionnaireStateService.selectedTrainigTypes = [...this.coachQuestionnaireStateService.selectedTrainigTypes, trainingType];
+    }
+  }
 
   public handleSubmitButtonClick(evt: Event) {
     evt.preventDefault();
@@ -21,5 +41,7 @@ export class SignUpQuestionnaireCoachComponent {
     console.log(this.signUpStateService.password);
     console.log(this.signUpStateService.gender);
     console.log(this.signUpStateService.userRole);
+
+    console.log(this.selectedTrainigTypes);
   }
 }
